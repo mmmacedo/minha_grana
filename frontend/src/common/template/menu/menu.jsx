33cc532be335d73a49897a50b4e016d29558375c
mmmacedo <murilommacedo666@gmail.com>
2019-05-05 11:15:21 -0300

    criada entrada de arvore de menu

--- frontend/src/common/template/menu/menu.jsx
+++ frontend/src/common/template/menu/menu.jsx
@@ -1,8 +1,16 @@
 import React from 'react';
 import MenuItem from '../menuItem/menuItem';
+import MenuTree from '../menuTree/menuTree';
 
 export default props => (
     <ul className='sidebar-menu'>
         <MenuItem path='#' label='Dashboard' icon='dashboard'/>
+        <MenuTree label='Cadastro' icon='edit'>
+            <MenuItem
+                path='#billingCycles'
+                label='Ciclo de Pagamentos'
+                icon='usd'
+            />
+        </MenuTree>
     </ul>
 )
