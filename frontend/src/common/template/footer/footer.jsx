321f37c52c602047ba60420deee624f27cb0232f
mmmacedo <murilommacedo666@gmail.com>
2019-05-05 11:23:41 -0300

    adicionado footer

---
+++ frontend/src/common/template/footer/footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default props => (
+ <footer className='main-footer'>
+     <strong>
+         HUe hue hue
+         <a href='http://www.google.com' target='_blank'> Google </a>
+     </strong>
+ </footer>
+)
--- frontend/src/main/app.jsx
+++ frontend/src/main/app.jsx
@@ -3,14 +3,15 @@ import '../common/template/dependencies';
 
 import Header from '../common/template/header/header';
 import Sidebar from '../common/template/sidebar/sidebar';
-
+import Footer from '../common/template/footer/footer';
 
 export default props => (
     <div className='wrapper'>
         <Header/>
         <Sidebar/>
         <div className='content-wrapper'>
-            <h1>Teste App</h1>
+            <h1>App</h1>
         </div>
+        <Footer/>
     </div>
 )
